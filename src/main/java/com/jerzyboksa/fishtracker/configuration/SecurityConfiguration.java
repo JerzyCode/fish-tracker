ddae7878a107435da5752c609373c017dca86232
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-08-25 09:39:53 +0200

    add basics to app

---
+++ src/main/java/com/jerzyboksa/fishtracker/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.jerzyboksa.fishtracker.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(request -> request
+            .requestMatchers("/h2-console/**").permitAll()
+            .anyRequest().authenticated())
+        .headers(header -> header
+            .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+        .build();
+  }
+}
