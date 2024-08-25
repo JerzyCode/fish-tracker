7fa6db79ad95b28c98d85bcc73d3d15e3c91bb18
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-08-25 07:39:50 +0200

    init

---
+++ src/main/java/com/jerzyboksa/fishtracker/FishTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jerzyboksa.fishtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishTrackerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FishTrackerApplication.class, args);
+  }
+
+}
